2ab061da5a0e85a4775976c84fc2638335fe4160
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2025-04-29 04:07:32 +0500

    chore: install eslint plugin perfectionist

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,13 @@
 import eslint from '@eslint/js';
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 import tseslint from 'typescript-eslint';
+import perfectionist from 'eslint-plugin-perfectionist'
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.strictTypeChecked,
   tseslint.configs.stylisticTypeChecked,
+  perfectionist.configs['recommended-natural'],
   { ignores: ['dist', 'node_modules', '**/*config.js', '**/*.spec.ts'] },
   { linterOptions: { noInlineConfig: true, reportUnusedDisableDirectives: true } },
   {
