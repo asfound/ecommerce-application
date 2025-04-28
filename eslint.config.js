231514c1d2ff427c46499f9ca931c04fc28402bb
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2025-04-29 04:20:29 +0500

    chore: install eslint plugin import

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import eslint from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import eslintPluginImport from 'eslint-plugin-import';
 import perfectionist from 'eslint-plugin-perfectionist';
 import eslintPluginUnicorn from 'eslint-plugin-unicorn';
 import tseslint from 'typescript-eslint';
@@ -10,6 +11,7 @@ export default tseslint.config(
   tseslint.configs.stylisticTypeChecked,
   perfectionist.configs['recommended-natural'],
   eslintPluginUnicorn.configs.recommended,
+  eslintPluginImport.flatConfigs.recommended,
   { ignores: ['dist', 'node_modules', '**/*config.js', '**/*.spec.ts'] },
   { linterOptions: { noInlineConfig: true, reportUnusedDisableDirectives: true } },
   {
@@ -21,7 +23,21 @@ export default tseslint.config(
     },
   },
   {
+    settings: {
+      'import/parsers': {
+        '@typescript-eslint/parser': ['.ts'],
+      },
+      'import/resolver': {
+        typescript: {
+          alwaysTryTypes: true,
+          project: './tsconfig.json',
+        },
+      },
+    },
+  },
+  {
     rules: {
+      'import/no-cycle': 'error',
       'unicorn/no-null': 'off',
       'unicorn/prevent-abbreviations': ['error', { allowList: { env: true } }],
     },
