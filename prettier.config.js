422a3e8bbf0a5662d61b37bf1d0edacc6fe0bf97
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2025-04-29 03:44:12 +0500

    chore: add prettier config

---
+++ prettier.config.js
@@ -0,0 +1,17 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  printWidth: 100,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  singleQuote: true,
+  semi: true,
+  bracketSpacing: true,
+  bracketSameLine: true,
+  arrowParens: "always",
+};
+
+export default config;
