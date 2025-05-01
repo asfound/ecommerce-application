4d03c646f805978b09f6a5c1b45e2c48241c3c07
Alena Radomskaia <radomskaia.a.i@gmail.com>
2025-05-01 14:24:31 +0700

    feat(test): integrate Vitest for testing and add coverage reports

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -17,4 +17,12 @@ export default defineConfig({
       '~': path.resolve(__dirname, './src'),
     },
   },
+
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    coverage: {
+      reporter: ['text', 'html', 'lcov'],
+    },
+  }
 });
