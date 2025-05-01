69ee7db1cc3833a02f11f079067ad1d86f32ee0a
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2025-05-01 23:55:02 +0500

    chore: update lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -4,6 +4,6 @@
  */
 export default {
   'src/**/*.{js,ts,cjs,mjs,json,md,css}': 'prettier --write --ignore-unknown',
-  'src/**/*.{js,ts,cjs,mjs}': 'eslint --fix --max-warnings 0',
+  'src/**/*.{js,ts,cjs,mjs}': 'eslint --fix --max-warnings 0 --no-warn-ignored',
   'src/**/*.css': 'stylelint --fix',
 };
