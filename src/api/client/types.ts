af9ff72992e6de8c2f3251419700a2bba416f6ae
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2025-05-02 01:06:56 +0500

    feat: add client builder

---
+++ src/api/client/types.ts
@@ -0,0 +1,8 @@
+import type { TokenCache, UserAuthOptions } from '@commercetools/ts-client';
+
+import type { AUTH_FLOW_TYPE } from './constants';
+
+export type ApiBuilderOptions =
+  | { refreshToken: string; tokenCache: TokenCache; type: typeof AUTH_FLOW_TYPE.REFRESH }
+  | { tokenCache: TokenCache; type: typeof AUTH_FLOW_TYPE.ANONYMOUS }
+  | { tokenCache: TokenCache; type: typeof AUTH_FLOW_TYPE.PASSWORD; user: UserAuthOptions };
