4d41bce8106c2bf95f43d05ffc4e43e39ad6e028
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2025-05-02 21:17:07 +0500

    refactor: remove nonnulable predicate

--- src/api/helpers/helpers.ts
+++ src/api/helpers/helpers.ts
@@ -1,16 +1,12 @@
 import type { TokenStore } from '@commercetools/ts-client';
 
-export const isNonNullable = <TValue>(value: TValue): value is NonNullable<TValue> => {
-  return value !== null && value !== undefined;
-};
-
 export const isObject = (value: unknown): value is object => {
   return typeof value === 'object';
 };
 
 export const isTokenStore = (value: unknown): value is TokenStore => {
   return (
-    isNonNullable(value) &&
+    value != null &&
     isObject(value) &&
     Reflect.has(value, 'token') &&
     Reflect.has(value, 'expirationTime') &&
