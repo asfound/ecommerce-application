73268bfeed6a08ba31928f391226c29661593905
Alena Radomskaia <radomskaia.a.i@gmail.com>
2025-05-03 18:35:11 +0700

    refactor(workflows): unify text template handling and clean up unused functions

--- .github/scripts/send-telegram-message.sh
+++ .github/scripts/send-telegram-message.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 set -e
 
-#escape_md2() {
-#    local text="$1"
-#    local chars=( \\ _ "*" "[" "]" "(" ")" "~" "\`" ">" "<" "&" "#" "+" "-" "=" "|" "{" "}" "." "!" )
-#
-#    for char in "${chars[@]}"; do
-#        text=${text//"$char"/\\$char}
-#    done
-#
-#    echo "$text"
-#}
-
 echo "${MESSAGE}"
-
-#SAFE_MESSAGE=$(escape_md2 "${MESSAGE}")
 
 curl -s -X POST https://api.telegram.org/bot"${TG_BOT_TOKEN}"/sendMessage \
   -d chat_id="${TG_CHAT_ID}" \
