1ab0855410e96623adfc0a1ad76d9a865a8053ab
ripetchor <115036520+ripetchor@users.noreply.github.com>
2025-05-03 19:16:57 +0500

    feat(RSS-ECOMM-1_30): add predicates for native types (#73)
    * refactor: rename folder
    
    * feat: add predicates for base types

--- src/shared/types/type-predicates.ts
+++
@@ -1,9 +0,0 @@
-export function isNumber(value: unknown): value is number {
-  return typeof value === 'number' && !Number.isNaN(value);
-}
-
-export function isString(value: unknown): value is string {
-  return typeof value === 'string';
-}
-
-// other predicates to be added
