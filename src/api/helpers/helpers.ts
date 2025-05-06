3cdcd89a4fd3742c028175e8ff4157d696ed7c51
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2025-05-07 01:46:47 +0500

    feat: add isSuccessResponse helper function

--- src/api/helpers/helpers.ts
+++ src/api/helpers/helpers.ts
@@ -1,7 +1,16 @@
-import type { ByProjectKeyRequestBuilder } from '@commercetools/platform-sdk';
+import type { ByProjectKeyRequestBuilder, ClientResponse } from '@commercetools/platform-sdk';
 
 import { ApiBuilder } from '../client/api-builder';
+import { HTTP_STATUS_CODE } from '../constants/constants';
 
 export const getApiRoot = (): ByProjectKeyRequestBuilder => {
   return ApiBuilder.instance.apiRoot;
+};
+
+export const isSuccessResponse = <T>(response: ClientResponse<T>): boolean => {
+  return (
+    response.statusCode != null &&
+    response.statusCode >= HTTP_STATUS_CODE.OK_MIN &&
+    response.statusCode < HTTP_STATUS_CODE.OK_MAX
+  );
 };
