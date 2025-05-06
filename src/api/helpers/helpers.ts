d2a6a4dae039446a1927ec813569f4d3b3dd1bef
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2025-05-07 01:24:41 +0500

    chore: remove isObject predicate function

--- src/api/helpers/helpers.ts
+++ src/api/helpers/helpers.ts
@@ -2,10 +2,6 @@ import type { ByProjectKeyRequestBuilder } from '@commercetools/platform-sdk';
 
 import { ApiBuilder } from '../client/api-builder';
 
-export const isObject = (value: unknown): value is object => {
-  return typeof value === 'object';
-};
-
 export const getApiRoot = (): ByProjectKeyRequestBuilder => {
   return ApiBuilder.instance.apiRoot;
 };
