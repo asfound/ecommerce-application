9a499de1b7a1f501a557b4239464b5e8515f7a75
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2025-05-07 01:24:21 +0500

    feat: add getApiRoot helper function

--- src/api/helpers/helpers.ts
+++ src/api/helpers/helpers.ts
@@ -1,3 +1,11 @@
+import type { ByProjectKeyRequestBuilder } from '@commercetools/platform-sdk';
+
+import { ApiBuilder } from '../client/api-builder';
+
 export const isObject = (value: unknown): value is object => {
   return typeof value === 'object';
+};
+
+export const getApiRoot = (): ByProjectKeyRequestBuilder => {
+  return ApiBuilder.instance.apiRoot;
 };
