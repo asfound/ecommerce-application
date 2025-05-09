c111161ad4b1ddb983dc9f78824851e5cfbc6320
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2025-05-09 22:23:51 +0500

    feat: add common button component

---
+++ src/components/common/button/button.ts
@@ -0,0 +1,37 @@
+import { BaseComponent } from '~/components/base-component/base-component';
+
+export interface ButtonProperties {
+  onClick(): void;
+  textContent: string;
+  type: HTMLButtonElement['type'];
+}
+
+export class Button extends BaseComponent<HTMLButtonElement> {
+  private readonly properties;
+
+  public constructor(properties: ButtonProperties) {
+    super({
+      attributes: { type: properties.type },
+      tagName: 'button',
+      textContent: properties.textContent,
+    });
+
+    this.properties = properties;
+
+    this.setupListeners();
+  }
+
+  public disable(): void {
+    this.setAttributes({ disabled: true });
+  }
+
+  public enable(): void {
+    this.removeAttribute('disabled');
+  }
+
+  private setupListeners(): void {
+    this.addListener('click', () => {
+      this.properties.onClick();
+    });
+  }
+}
