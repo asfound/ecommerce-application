2042f2167ed7e10efdac575183c7452741f87c58
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2025-05-09 18:57:07 +0500

    feat: add pasword validators

--- src/feature/login-form/login-form.view.ts
+++ src/feature/login-form/login-form.view.ts
@@ -5,7 +5,17 @@ import { button, div, h2 } from '~/shared/create-element/tags';
 
 import { Input } from './input';
 import styles from './login-form.module.css';
-import { required, validateEmailFormat, validateNoSpaces } from './validators';
+import {
+  PASSWORD_LENGTH,
+  validateEmailFormat,
+  validateHasDigit,
+  validateHasLowercase,
+  validateHasUppercase,
+  validateMinLength,
+  validateNoSpaces,
+  validateOnlyEnglishLetters,
+  validateRequired,
+} from './validators';
 
 export class LoginFormView extends BaseComponent<HTMLFormElement> implements Component {
   public constructor() {
@@ -25,13 +35,22 @@ export class LoginFormView extends BaseComponent<HTMLFormElement> implements Com
       name: 'email',
       placeholder: 'Email',
       type: 'text',
-      validators: [required, validateNoSpaces, validateEmailFormat],
+      validators: [validateRequired, validateNoSpaces, validateEmailFormat],
     });
 
     const inputPassword = new Input({
       name: 'password',
       placeholder: 'Password',
       type: 'password',
+      validators: [
+        validateRequired,
+        validateNoSpaces,
+        validateMinLength(PASSWORD_LENGTH),
+        validateHasUppercase,
+        validateHasLowercase,
+        validateHasDigit,
+        validateOnlyEnglishLetters,
+      ],
     });
 
     const buttonSubmit = button({ className: styles.button, type: 'submit' }, 'Log in');
--- src/feature/login-form/validators.ts
+++ src/feature/login-form/validators.ts
@@ -1,14 +1,21 @@
 import type { ValidatorFunction } from './input';
 
 const EMAIL_VALIDATION_ERROR = {
-  INCORRECT_FORMAT:
-    'Please enter your email address in the correct format (e.g., user@example.com)',
+  INCORRECT_FORMAT: 'Please enter email in the correct format (e.g., user@example.com)',
   WHITESPACES: 'Email must not contain white spaces',
 } as const;
 
 const VALIDATION_ERROR = {
+  HAS_DIGIT: 'Must contain at least one digit (0–9).',
+  HAS_LOWERCASE: 'Must contain at least one lowercase letter (a–z).',
+  HAS_UPPERCASE: 'Must contain at least one uppercase letter (A–Z).',
+  MIN_LENGTH: (required: number, current: number) =>
+    `Minimum required length is ${required.toString()} characters. You entered ${current.toString()}`,
+  ONLY_ENGLISH_LETTERS: 'Only English letters (A–Z, a–z) are allowed.',
   REQUIRED: 'Field is required',
 } as const;
+
+export const PASSWORD_LENGTH = 8;
 
 export const validateEmailFormat: ValidatorFunction = (value) => {
   const emailRegex = /^[^@]+@[^@]+\.[^@.]{2,}$/;
@@ -21,6 +28,29 @@ export const validateNoSpaces: ValidatorFunction = (value) => {
   return hasLeadingOrTrailingSpaces || hasSpaces ? EMAIL_VALIDATION_ERROR.WHITESPACES : null;
 };
 
-export const required: ValidatorFunction = (value) => {
+export const validateRequired: ValidatorFunction = (value) => {
   return value.length === 0 ? VALIDATION_ERROR.REQUIRED : null;
+};
+
+export const validateMinLength = (length: number) => {
+  return (value: string): null | string => {
+    const currentLength = value.trim().length;
+    return currentLength < length ? VALIDATION_ERROR.MIN_LENGTH(length, currentLength) : null;
+  };
+};
+
+export const validateHasUppercase: ValidatorFunction = (value) => {
+  return /[A-Z]/.test(value) ? null : VALIDATION_ERROR.HAS_UPPERCASE;
+};
+
+export const validateHasLowercase: ValidatorFunction = (value) => {
+  return /[a-z]/.test(value) ? null : VALIDATION_ERROR.HAS_LOWERCASE;
+};
+
+export const validateHasDigit: ValidatorFunction = (value) => {
+  return /\d/.test(value) ? null : VALIDATION_ERROR.HAS_DIGIT;
+};
+
+export const validateOnlyEnglishLetters: ValidatorFunction = (value) => {
+  return /^[a-zA-Z0-9]+$/.test(value) ? null : VALIDATION_ERROR.ONLY_ENGLISH_LETTERS;
 };
