c733412189d4c01d92203f2a2435087dcea155f1
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2025-05-10 15:33:43 +0500

    chore: add dots to error messages

--- src/shared/constants/constants.ts
+++ src/shared/constants/constants.ts
@@ -1,5 +1,5 @@
 export const EMAIL_VALIDATION_ERROR = {
-  INCORRECT_FORMAT: 'Please enter email in the correct format (e.g., user@example.com)',
+  INCORRECT_FORMAT: 'Please enter email in the correct format (e.g., user@example.com).',
 } as const;
 
 export const VALIDATION_ERROR = {
@@ -7,10 +7,10 @@ export const VALIDATION_ERROR = {
   HAS_LOWERCASE: 'Must contain at least one lowercase letter (a–z).',
   HAS_UPPERCASE: 'Must contain at least one uppercase letter (A–Z).',
   MIN_LENGTH: (required: number, current: number) =>
-    `Minimum required length is ${required.toString()} characters. You entered ${current.toString()}`,
+    `Minimum required length is ${required.toString()} characters. You entered ${current.toString()}.`,
   ONLY_ENGLISH_LETTERS: 'Only English letters (A–Z, a–z) are allowed.',
-  REQUIRED: 'Field is required',
-  WHITESPACES: 'Value must not contain white spaces',
+  REQUIRED: 'Field is required.',
+  WHITESPACES: 'Value must not contain white spaces.',
 } as const;
 
 export const REQUIRED_PASSWORD_LENGTH = 8;
