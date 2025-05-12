27febf3fcf6203c3bbfc0a677d1bc2d008cdbfac
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2025-05-13 00:48:22 +0500

    chore: add auth urls

--- src/api/msw-mocks/handlers.ts
+++ src/api/msw-mocks/handlers.ts
@@ -1,1 +1,5 @@
+import { API_URL, PROJECT_KEY } from '../client/constants';
+
+export const LOGIN_URL = `${API_URL}/${PROJECT_KEY}/me/login`;
+export const SIGNUP_URL = `${API_URL}/${PROJECT_KEY}/me/signup`;
 export const handlers = [];
