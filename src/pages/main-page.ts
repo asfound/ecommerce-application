ca471096f5b5b6195b8c28393200425e013f126c
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2025-05-14 16:24:28 +0500

    feat: add login and registration buttons to main page

--- src/pages/main-page.ts
+++ src/pages/main-page.ts
@@ -1,12 +1,60 @@
+import { ROUTE_PATH } from '~/app/router/route-path';
+import { Router } from '~/app/router/router';
+import { rootSelector } from '~/app/store/selectors';
+import { rootStore } from '~/app/store/store';
 import { BaseComponent } from '~/components/base-component/base-component';
+import { Button } from '~/components/common/button/button';
 import { HeroBanner } from '~/feature/hero-banner/hero-banner';
+import { div } from '~/shared/create-element/tags';
+
+import styles from './main-page.module.css';
 
 export class MainPage extends BaseComponent {
+  // TODO: remove after sprint-2
+  private readonly unsubscribeButton: VoidFunction;
+
   public constructor() {
     super({ tagName: 'main' });
 
     const heroBanner = new HeroBanner();
 
-    this.append(heroBanner);
+    // TODO: remove after sprint-2
+    const buttonLogin = new Button({
+      onClick(): void {
+        Router.instance.navigate(ROUTE_PATH.LOGIN);
+      },
+      textContent: 'Login',
+      type: 'button',
+    });
+    const buttonRegistration = new Button({
+      onClick(): void {
+        Router.instance.navigate(ROUTE_PATH.REGISTRATION);
+      },
+      textContent: 'Registration',
+      type: 'button',
+    });
+    const buttonsContainer = div(
+      { className: styles.buttonsContainer },
+      buttonLogin.element,
+      buttonRegistration.element,
+    );
+
+    this.unsubscribeButton = rootStore.subscribe(rootSelector.selectLoggedIn, (loggedIn) => {
+      if (loggedIn) {
+        buttonLogin.disable();
+      } else {
+        buttonLogin.enable();
+      }
+    });
+    // ================================
+
+    this.append(heroBanner, buttonsContainer);
+  }
+
+  // TODO: remove after sprint-2
+  public override destroy(): void {
+    this.unsubscribeButton();
+
+    super.destroy();
   }
 }
