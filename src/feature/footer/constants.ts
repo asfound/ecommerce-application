f3b757a6a2a84e035cccfc4abda55efccc07f6a5
asfound <fffound27@gmail.com>
2025-05-14 14:31:25 +0200

    refactor: use constants

---
+++ src/feature/footer/constants.ts
@@ -0,0 +1,56 @@
+import facebook from '~/assets/icons/facebook.svg';
+import instagram from '~/assets/icons/instagram.svg';
+import pinterest from '~/assets/icons/pinterest.svg';
+import telegram from '~/assets/icons/telegram.svg';
+import twitter from '~/assets/icons/twitter.svg';
+
+export const FOOTER_INFO = {
+  COPYRIGHT: {
+    CREDITS: '© 2025 HUH Coffee',
+    POLICY: 'Privacy policy',
+    TERMS: 'Terms and conditions',
+  },
+  CREDENTIALS: {
+    DESCRIPTION: 'Discover the richness of nature in our coffee products.',
+    EMAIL: 'info@huh-coffee.top',
+    LOCATION: 'London, United Kingdom',
+    MOBILE: '+12 050 123 45 67',
+  },
+  LISTS: {
+    ACCOUNT: 'My account',
+    CARE: 'Customer care',
+    HELP: 'Help',
+  },
+  SUBSCRIPTION: {
+    CTA: 'Subscribe →',
+    DESCRIPTION: 'Stay informed, subscribe to our newsletter now!',
+    TITLE: 'Sign up for emails',
+  },
+};
+
+export const HELP_LINKS_TEXT = {
+  CONTACT: 'Contact us',
+  FAQ: 'FAQ',
+  SHIPPING: 'Shipping & Returns',
+};
+
+export const ACCOUNT_LINKS_TEXT = {
+  ADDRESSES: 'Addresses',
+  ORDER_STATUS: 'Order Status',
+  WISHLIST: 'Wishlist',
+};
+
+export const CARE_LINKS_TEXT = {
+  ABOUT: 'About us',
+  BLOG: 'Blog',
+};
+
+export const SOCIAL_LINKS = [
+  { href: 'https://facebook.com', icon: facebook },
+  { href: 'https://pinterest.com', icon: pinterest },
+  { href: 'https://telegram.com', icon: telegram },
+  { href: 'https://instagram.com', icon: instagram },
+  { href: 'https://twitter.com', icon: twitter },
+];
+
+export const SUBSCRIPTION_SUCCESS = "You've been successfully subscribed!";
--- src/feature/footer/footer.ts
+++ src/feature/footer/footer.ts
@@ -1,10 +1,5 @@
 import type { Component } from '~/components/base-component/types';
 
-import facebook from '~/assets/icons/facebook.svg';
-import instagram from '~/assets/icons/instagram.svg';
-import pinterest from '~/assets/icons/pinterest.svg';
-import telegram from '~/assets/icons/telegram.svg';
-import twitter from '~/assets/icons/twitter.svg';
 import { BaseComponent } from '~/components/base-component/base-component';
 import { Input } from '~/components/common/input/input';
 import { Logo } from '~/components/logo/logo';
@@ -14,58 +9,15 @@ import { a, div, li, p, ul } from '~/shared/create-element/tags';
 import { createSvgIcon } from '~/shared/utils/create-svg';
 import { showToast } from '~/shared/utils/show-toast';
 
+import {
+  ACCOUNT_LINKS_TEXT,
+  CARE_LINKS_TEXT,
+  FOOTER_INFO,
+  HELP_LINKS_TEXT,
+  SOCIAL_LINKS,
+  SUBSCRIPTION_SUCCESS,
+} from './constants';
 import styles from './footer.module.css';
-
-export const FOOTER_INFO = {
-  COPYRIGHT: {
-    CREDITS: '© 2025 HUH Coffee',
-    POLICY: 'Privacy policy',
-    TERMS: 'Terms and conditions',
-  },
-  CREDENTIALS: {
-    DESCRIPTION: 'Discover the richness of nature in our coffee products.',
-    EMAIL: 'info@huh-coffee.top',
-    LOCATION: 'London, United Kingdom',
-    MOBILE: '+12 050 123 45 67',
-  },
-  LISTS: {
-    ACCOUNT: 'My account',
-    CARE: 'Customer care',
-    HELP: 'Help',
-  },
-  SUBSCRIPTION: {
-    CTA: 'Subscribe →',
-    DESCRIPTION: 'Stay informed, subscribe to our newsletter now!',
-    TITLE: 'Sign up for emails',
-  },
-};
-
-export const HELP_LINKS_TEXT = {
-  CONTACT: 'Contact us',
-  FAQ: 'FAQ',
-  SHIPPING: 'Shipping & Returns',
-};
-
-export const ACCOUNT_LINKS_TEXT = {
-  ADDRESSES: 'Addresses',
-  ORDER_STATUS: 'Order Status',
-  WISHLIST: 'Wishlist',
-};
-
-export const CARE_LINKS_TEXT = {
-  ABOUT: 'About us',
-  BLOG: 'Blog',
-};
-
-export const SOCIAL_LINKS = [
-  { href: 'https://facebook.com', icon: facebook },
-  { href: 'https://pinterest.com', icon: pinterest },
-  { href: 'https://telegram.com', icon: telegram },
-  { href: 'https://instagram.com', icon: instagram },
-  { href: 'https://twitter.com', icon: twitter },
-];
-
-export const SUBSCRIPTION_SUCCESS = "You've been successfully subscribed!";
 
 export class Footer extends BaseComponent implements Component {
   public constructor() {
