95066544a7550a23bc6b470c2483151a4cef6f50
ripetchor <115036520+ripetchor@users.noreply.github.com>
2025-05-15 22:15:47 +0500

    fix(RSS-ECOMM-2_50): perform self crosscheck and fix any issues (#168)
    * refactor: remove logout call from login method
    
    * feat: use thin scrollbar
    
    * fix: scroll to top after signup
    
    * fix: unlock login link and button
    
    * fix: add scroll to top

--- src/pages/main-page.ts
+++ src/pages/main-page.ts
@@ -1,7 +1,5 @@
 import { ROUTE_PATH } from '~/app/router/route-path';
 import { Router } from '~/app/router/router';
-import { rootSelector } from '~/app/store/selectors';
-import { rootStore } from '~/app/store/store';
 import { BaseComponent } from '~/components/base-component/base-component';
 import { Button } from '~/components/common/button/button';
 import { HeroBanner } from '~/feature/hero-banner/hero-banner';
@@ -10,9 +8,6 @@ import { div } from '~/shared/create-element/tags';
 import styles from './main-page.module.css';
 
 export class MainPage extends BaseComponent {
-  // TODO: remove after sprint-2
-  private readonly unsubscribeButton: VoidFunction;
-
   public constructor() {
     super({ tagName: 'main' });
 
@@ -38,23 +33,8 @@ export class MainPage extends BaseComponent {
       buttonLogin.element,
       buttonRegistration.element,
     );
-
-    this.unsubscribeButton = rootStore.subscribe(rootSelector.selectLoggedIn, (loggedIn) => {
-      if (loggedIn) {
-        buttonLogin.disable();
-      } else {
-        buttonLogin.enable();
-      }
-    });
     // ================================
 
     this.append(heroBanner, buttonsContainer);
-  }
-
-  // TODO: remove after sprint-2
-  public override destroy(): void {
-    this.unsubscribeButton();
-
-    super.destroy();
   }
 }
