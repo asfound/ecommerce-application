26c9a4b65c154180f8ee9c53458373fd15901fe3
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2025-05-16 15:06:51 +0500

    chore: remove comment

--- src/api/services/auth/auth.service.ts
+++ src/api/services/auth/auth.service.ts
@@ -62,7 +62,6 @@ export class AuthService {
       password: payload.password,
     };
 
-    // REAL REQUEST
     const response = await this.apiRoot().me().login().post({ body }).execute();
 
     if (isSuccessResponse(response)) {
