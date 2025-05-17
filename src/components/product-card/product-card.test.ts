bd86c1d923224fc9544d3bbf94d383773cce0043
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2025-05-17 17:30:34 +0500

    chore: add tests

---
+++ src/components/product-card/product-card.test.ts
@@ -0,0 +1,17 @@
+import { ProductCard } from './product-card';
+
+const productCard = new ProductCard({
+  description: 'test',
+  image: { label: 'test', url: 'test' },
+  name: 'test',
+  price: { default: 0 },
+  sku: 'test',
+});
+
+test('productCard should be defined', () => {
+  expect(productCard).toBeDefined();
+});
+
+test('productCard should be instance of ProductCard', () => {
+  expect(productCard).toBeInstanceOf(ProductCard);
+});
