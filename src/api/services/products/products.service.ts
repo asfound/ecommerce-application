7e6cd407ce96dc74a26daa3ee0264625ea6b0d16
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2025-05-17 16:16:19 +0500

    chore: add temp sort

--- src/api/services/products/products.service.ts
+++ src/api/services/products/products.service.ts
@@ -1,5 +1,7 @@
 import type { ApiRootGetter } from '~/api/types/types';
 
+import { APP_LOCALE } from '~/shared/constants/constants';
+
 import type { AppProduct } from './types';
 
 import { mapToAppProducts } from './mappers';
@@ -19,9 +21,11 @@ export class ProductsService {
   }
 
   public async getProducts(payload: { limit: number }): Promise<AppProduct[]> {
+    const TEMPORARY_SORT = `name.${APP_LOCALE} asc`;
+
     const response = await this.apiRoot()
       .productProjections()
-      .get({ queryArgs: { limit: payload.limit } })
+      .get({ queryArgs: { limit: payload.limit, sort: TEMPORARY_SORT } })
       .execute();
 
     return mapToAppProducts(response.body.results);
