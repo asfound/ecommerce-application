86d4176e6d5debb72fa0d62ef71662a378278c51
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2025-05-19 02:20:50 +0500

    fix: fix replaceChildren

--- src/components/base-component/base-component.ts
+++ src/components/base-component/base-component.ts
@@ -80,10 +80,6 @@ export class BaseComponent<TElementType extends HTMLElement = HTMLElement> {
   }
 
   public destroyChildren(): void {
-    if (this.children.size === 0) {
-      return;
-    }
-
     for (const child of this.children) {
       child.destroy();
     }
