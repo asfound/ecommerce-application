6d9c6106e773d40d1588abc6b9b6c50de99dc824
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2025-05-21 18:58:59 +0500

    feat: add filter and input number components

---
+++ src/components/common/input/input-number/input-number.ts
@@ -0,0 +1,20 @@
+import { INPUT_TYPE } from '~/shared/constants/constants';
+
+import type { InputBaseProperties } from '../input-base';
+
+import { InputBase } from '../input-base';
+import styles from './input-number.module.css';
+
+export class InputNumber extends InputBase {
+  public get value(): string {
+    return this.inputComponent.element.value;
+  }
+
+  public constructor(properties: InputBaseProperties) {
+    super(properties);
+
+    this.inputComponent.setAttributes({ type: INPUT_TYPE.NUMBER });
+
+    this.inputComponent.addClassNames(styles.input);
+  }
+}
---
+++ src/components/filter/filter.ts
@@ -0,0 +1,81 @@
+import iconArrowUp from '~/assets/icons/arrow-up.svg';
+import { div, summary } from '~/shared/create-element/tags';
+import { createSvgIcon } from '~/shared/utils/create-svg';
+
+import type { Component } from '../base-component/types';
+
+import { BaseComponent } from '../base-component/base-component';
+import { InputNumber } from '../common/input/input-number/input-number';
+import styles from './filter.module.css';
+
+export type FilterProperties = FilterCheckboxesProperties | FilterPriceRangeProperties;
+
+interface FilterCheckboxesProperties {
+  title: string;
+  type: 'checkboxes';
+}
+
+interface FilterPriceRangeProperties {
+  onMaxPriceChange(maxPrice: string): void;
+  onMinPriceChange(minPrice: string): void;
+  title: string;
+  type: 'price-range';
+}
+
+export class Filter extends BaseComponent<HTMLDetailsElement> implements Component {
+  private readonly arrowIcon = createSvgIcon(iconArrowUp, styles.icon);
+
+  private readonly titleElement = div({ className: styles.title });
+
+  private readonly summaryElement = summary(
+    { className: styles.summary },
+    this.titleElement,
+    this.arrowIcon,
+  );
+
+  public constructor() {
+    super({
+      attributes: { open: true },
+      className: ['FILTER', styles.container],
+      tagName: 'details',
+    });
+
+    this.setupListeners();
+  }
+
+  public createHTML(properties: FilterProperties): void {
+    this.titleElement.textContent = properties.title;
+
+    this.append(this.summaryElement);
+
+    if (properties.type === 'price-range') {
+      const inputMinPrice = new InputNumber({ name: 'min-price', placeholder: 'From' });
+      inputMinPrice.addListener('input', () => {
+        properties.onMinPriceChange(inputMinPrice.value);
+      });
+
+      const inputMaxPrice = new InputNumber({ name: 'max-price', placeholder: 'To' });
+      inputMaxPrice.addListener('input', () => {
+        properties.onMaxPriceChange(inputMaxPrice.value);
+      });
+
+      const pricesContainer = div(
+        { className: styles.priceRangeContainer },
+        inputMinPrice.element,
+        inputMaxPrice.element,
+      );
+
+      this.append(pricesContainer);
+    }
+  }
+
+  private setupListeners(): void {
+    this.addListener('toggle', () => {
+      if (this.element.open) {
+        this.arrowIcon.classList.add(styles.rotated);
+      } else {
+        this.arrowIcon.classList.remove(styles.rotated);
+      }
+    });
+  }
+}
