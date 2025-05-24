28245bd4d41e864d46f3a90b75d2eece6d24a2ab
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2025-05-24 14:40:48 +0500

    feat: add scroll to top button

--- src/pages/catalog-page.ts
+++ src/pages/catalog-page.ts
@@ -1,4 +1,7 @@
+import { debounce } from 'lodash';
+
 import { SERVICE_HUB } from '~/api/services/service-hub';
+import iconUp from '~/assets/icons/arrow-up.svg';
 import { BaseComponent } from '~/components/base-component/base-component';
 import { IntersectionLoader } from '~/components/intersection-loader/intersection-loader';
 import { CategoryNavigationPresenter } from '~/feature/catalog/category-navigation/category-navigation.presenter';
@@ -11,11 +14,20 @@ import { SearchAndSortPresenter } from '~/feature/catalog/search-and-sort/search
 import { SearchAndSortView } from '~/feature/catalog/search-and-sort/search-and-sort.view';
 import { catalogStore } from '~/feature/catalog/store/store';
 import { CSS_CLASS_NAME } from '~/shared/constants/constants';
-import { div } from '~/shared/create-element/tags';
+import { button, div } from '~/shared/create-element/tags';
+import { createSvgIcon } from '~/shared/utils/create-svg';
 
 import styles from './catalog-page.module.css';
 
+const SCROLL_DEBOUNCE_MILLISECONDS = 150;
+const SCROLL_Y_OFFSET = 600;
+
 export class CatalogPage extends BaseComponent {
+  private readonly buttonToTop = button(
+    { className: [styles.buttonToTop, styles.hidden] },
+    createSvgIcon(iconUp, styles.icon),
+  );
+
   private readonly categoryNavigationPresenter: CategoryNavigationPresenter;
 
   private readonly filtersPresenter: FiltersPresenter;
@@ -57,7 +69,9 @@ export class CatalogPage extends BaseComponent {
       this.intersectionAnchor.element,
     );
 
-    this.append(sidebarElement, mainContentElement);
+    this.append(sidebarElement, mainContentElement, this.buttonToTop);
+
+    this.setupListeners();
   }
 
   public override destroy(): void {
@@ -70,5 +84,27 @@ export class CatalogPage extends BaseComponent {
     catalogStore.reset();
 
     super.destroy();
+  }
+
+  private setupListeners(): void {
+    this.buttonToTop.addEventListener(
+      'click',
+      () => {
+        window.scrollTo({ top: 0 });
+      },
+      { signal: this.abortController.signal },
+    );
+
+    window.addEventListener(
+      'scroll',
+      debounce(() => {
+        if (window.scrollY >= SCROLL_Y_OFFSET) {
+          this.buttonToTop.classList.remove(styles.hidden);
+        } else {
+          this.buttonToTop.classList.add(styles.hidden);
+        }
+      }, SCROLL_DEBOUNCE_MILLISECONDS),
+      { signal: this.abortController.signal },
+    );
   }
 }
