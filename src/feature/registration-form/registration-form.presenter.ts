b0aa68e121a674aee24faef608be3d15e315fe0a
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2025-05-24 15:23:17 +0500

    chore: remove auto login

--- src/feature/registration-form/registration-form.presenter.ts
+++ src/feature/registration-form/registration-form.presenter.ts
@@ -3,7 +3,6 @@ import type { SignupPayload } from '~/api/services/auth/types';
 
 import { ROUTE_PATH } from '~/app/router/route-path';
 import { Router } from '~/app/router/router';
-import { rootAction } from '~/app/store/actions';
 import { Presenter } from '~/shared/presenter/presenter';
 import { isError } from '~/shared/type-predicates/type-predicates';
 import { showToast } from '~/shared/utils/show-toast';
@@ -37,19 +36,9 @@ export class RegistrationFormPresenter extends Presenter<RegistrationFormView> {
     this.authService
       .signup(payload)
       .then(() => {
-        return { email: payload.email, password: payload.password };
-      })
-      .then((credentials) => {
-        this.authService.logout();
-
-        return this.authService.login(credentials);
-      })
-      .then(() => {
         this.view.hideError();
 
-        Router.instance.navigate(ROUTE_PATH.MAIN);
-
-        rootAction.setLoggedIn(true);
+        Router.instance.navigate(ROUTE_PATH.LOGIN);
 
         showToast(REGISTRATION_FORM_TEXT.ACCOUNT_CREATED);
       })
