9ab85c4f6d0ead19b48ce0b0f0676f26e1fb5c74
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2025-05-26 22:30:11 +0500

    fix: pass productId intead of name

--- src/feature/catalog/product-card-list/product-card-list.presenter.ts
+++ src/feature/catalog/product-card-list/product-card-list.presenter.ts
@@ -58,7 +58,7 @@ export class ProductCardListPresenter extends Presenter<ProductCardListView> {
   private readonly handleNavigateToDetails = (product: AppProduct): void => {
     Router.instance.navigate(ROUTE_PATH.PRODUCT_DETAILS, {
       searchParameters: {
-        name: product.name,
+        id: product.productId,
         sku: product.sku,
       },
     });
