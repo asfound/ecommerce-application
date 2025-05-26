82510081d059776ba34c1c7b84653b6e54b05a2f
asfound <fffound27@gmail.com>
2025-05-26 21:21:42 +0200

    feat(RSS-ECOMM-3_10): add image slider for product images (#219)
    * feat: add image slider
    
    * feat: place navigation buttons outside of slider
    
    * fix: add missing tag

---
+++ src/components/image-slider/image-slider.ts
@@ -0,0 +1,65 @@
+import { Swiper } from 'swiper';
+import { Navigation } from 'swiper/modules';
+
+import type { AppProductImage } from '~/api/services/products/types';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+import sliderArrowSvg from '~/assets/icons/slider-arrow.svg';
+import { div, img } from '~/shared/create-element/tags';
+import { createSvgIcon } from '~/shared/utils/create-svg';
+
+import type { Component } from '../base-component/types';
+
+import { BaseComponent } from '../base-component/base-component';
+import styles from './image-slider.module.css';
+
+export class ImageSlider extends BaseComponent implements Component {
+  private readonly images: AppProductImage[];
+
+  public constructor(images: AppProductImage[]) {
+    super({ className: styles.slider, tagName: 'div' });
+
+    this.images = images;
+
+    this.createHTML();
+  }
+
+  public createHTML(): void {
+    const wrapper = div({ className: 'swiper-wrapper' });
+
+    for (const image of this.images) {
+      const slide = div({ className: 'swiper-slide' });
+      const imageElement = img({ className: styles.image, src: image.url });
+
+      slide.append(imageElement);
+      wrapper.append(slide);
+    }
+
+    const buttonPrevious = div(
+      { className: [styles.navigationButton] },
+      createSvgIcon(sliderArrowSvg, [styles.arrow, styles.previous]),
+    );
+
+    const buttonNext = div(
+      { className: [styles.navigationButton] },
+      createSvgIcon(sliderArrowSvg, styles.arrow),
+    );
+
+    const buttonsContainer = div({ className: styles.navigation }, buttonPrevious, buttonNext);
+
+    const swiper = div({ className: ['swiper', styles.swiper] }, wrapper);
+
+    new Swiper(swiper, {
+      modules: [Navigation],
+      navigation: {
+        nextEl: buttonNext,
+        prevEl: buttonPrevious,
+      },
+      slidesPerView: 1,
+    });
+
+    this.append(buttonsContainer, swiper);
+  }
+}
--- src/shared/utils/create-svg.ts
+++ src/shared/utils/create-svg.ts
@@ -1,4 +1,4 @@
-export function createSvgIcon(iconName: string, className?: string): SVGElement {
+export function createSvgIcon(iconName: string, className?: string | string[]): SVGElement {
   const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
   const use = document.createElementNS('http://www.w3.org/2000/svg', 'use');
 
@@ -6,7 +6,11 @@ export function createSvgIcon(iconName: string, className?: string): SVGElement 
   svg.append(use);
 
   if (className) {
-    svg.setAttribute('class', className);
+    if (Array.isArray(className)) {
+      svg.setAttribute('class', className.join(' '));
+    } else {
+      svg.setAttribute('class', className);
+    }
   }
 
   return svg;
