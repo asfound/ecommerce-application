c08e4d5ffa64deb385cbadb9b457ea7533ca6daa
asfound <fffound27@gmail.com>
2025-05-31 15:11:24 +0200

    refactor: remove unused const

--- src/components/new-address-form/constants.ts
+++
@@ -1,6 +0,0 @@
-export const FIELD_NAME = {
-  CITY: 'City:',
-  COUNTRY: 'Country:',
-  POSTCODE: 'Postal code:',
-  STREET: 'Street:',
-};
--- src/feature/user-profile/user-details/constants.ts
+++ src/feature/user-profile/user-details/constants.ts
@@ -4,6 +4,7 @@ export const FIELD_NAME = {
   FIRST_NAME: 'First name:',
   LAST_NAME: 'Last name:',
 };
+
 export const USER_NOTIFICATION = {
   INFORMATION_SUCCESS: 'Information has been successfully updated!',
   PASSWORD_SUCCESS: 'Password has been successfully updated!',
