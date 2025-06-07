599ce8695775fab2d886b8a438553b056fae7a7f
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2025-06-08 00:24:56 +0500

    refactor: update team data

--- src/shared/constants/team-data.ts
+++ src/shared/constants/team-data.ts
@@ -4,7 +4,7 @@ import avatarAlena from '~/assets/img/alena.jpg';
 import avatarKsyusha from '~/assets/img/ksyusha.png';
 import avatarMargo from '~/assets/img/margo.jpg';
 import avatarMikhail from '~/assets/img/mikhail.png';
-import avatarYulik from '~/assets/img/yulikk.jpg';
+import avatarYulik from '~/assets/img/yulikk.png';
 
 interface TeamData {
   EXTERNAL_SUPPORT: ProfileCardProperties[];
@@ -16,28 +16,32 @@ export const TEAM_DATA: TeamData = {
     {
       avatarUrl: avatarMargo,
       bio: 'Hi! I’m Meg. What began as a personal curiosity has grown into a deep passion - not just for front-end development, but for the people who make the journey meaningful. Becoming a developer, and being part of this course, first as a student, now as a mentor and curator, has shown me how powerful a supportive community can be. Watching others grow, overcome challenges, and discover their own love for coding is just as fulfilling as building something myself. I’m excited to keep learning, creating, and helping others do the same.',
-      contributions: undefined,
+      contributions: ['Code reviews', 'Invaluable advices', 'Warmth and support on tough days'],
       fullname: 'Meg Golubeva',
       github: { login: 'stardustmeg', url: 'https://github.com/stardustmeg' },
-      roles: ['Mentor', 'Helping hand'],
+      roles: ['😎 Mentor', '🤝 Helping hand', '🦉 Wise Owl'],
     },
     {
       avatarUrl: avatarYulik,
       bio: 'Hi! My name is Yulia. A couple of years ago, I moved from Belarus to Krakow. Before that, I worked as a 1C programmer, helping accountants and financiers solve puzzles. Over time, I got tired of creating repetitive reports and decided to dive into the world of frontend development. I absolutely love this course and am grateful to the people behind it.',
-      contributions: undefined,
+      contributions: [
+        'Endless calls on commercetools',
+        'A calm voice in the chaos',
+        'Consistently kind',
+      ],
       fullname: '	Yuliya Kursevich',
       github: { login: 'yulikK', url: 'https://github.com/yulikK' },
-      roles: ['SDK queen', 'Helping hand'],
+      roles: ['👸 CT-SDK queen', '🤝 Helping hand', '🦉 Wise Owl'],
     },
   ],
   MEMBERS: [
     {
       avatarUrl: avatarMikhail,
-      bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit veritatis temporibus asperiores, perspiciatis blanditiis ab similique non eum error minus praesentium veniam deserunt corrupti repellendus officiis quas tempora molestiae sit. Fugiat quae tempore atque asperiores magni id illo mollitia iste ipsam eaque deserunt in adipisci assumenda, facere neque repellendus error cumque! Officiis ducimus suscipit voluptatibus, nam quia cum harum soluta quis reiciendis illo aliquid id quasi, aliquam ipsam adipisci consectetur eum cumque doloribus perspiciatis excepturi fugiat assumenda cupiditate repellendus maxime.',
+      bio: "Hello! I'm Grim. Currently learning web development, focusing on HTML, CSS, JS, TS. I've always been interested in technology, and now I'm building projects to improve my coding skills. I'm especially excited about creating interactive and responsive websites. My goal is to become a front-end developer and work on real-world applications.",
       contributions: ['Login page', 'Catalog page', 'Product details page', 'About us page'],
       fullname: 'Mikhail Zubenko',
       github: { login: 'ripetchor', url: 'https://github.com/ripetchor' },
-      roles: ['UI', 'CT-SDK', 'API'],
+      roles: ['🎨 UI', '🔮 CT-SDK', '🛠️ API'],
     },
     {
       avatarUrl: avatarKsyusha,
@@ -52,7 +56,7 @@ export const TEAM_DATA: TeamData = {
       ],
       fullname: 'Ksenia Gorina',
       github: { login: 'asfound', url: 'https://github.com/asfound' },
-      roles: ['Team lead', 'UI', 'CT-SDK', 'API'],
+      roles: ['👑 Team lead', '🎨 UI', '🔮 CT-SDK', '🛠️ API'],
     },
     {
       avatarUrl: avatarAlena,
@@ -60,7 +64,7 @@ export const TEAM_DATA: TeamData = {
       contributions: ['CI/CD'],
       fullname: 'Alena Radomskaia',
       github: { login: 'radomskaia', url: 'https://github.com/radomskaia' },
-      roles: ['CI/CD', 'Ex-member'],
+      roles: ['📊 CI/CD', '😞 Ex-member'],
     },
   ],
 } as const;
