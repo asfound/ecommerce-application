2c2b2416b4058874c3dadbbe44d88bd2f5be889a
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2025-06-08 22:42:34 +0500

    feat: implement image alignment

--- src/feature/discount-codes/discount-codes.view.ts
+++ src/feature/discount-codes/discount-codes.view.ts
@@ -8,6 +8,8 @@ import { div, h2, h3, img, p } from '~/shared/create-element/tags';
 import { DISCOUNT_CODE_IMAGE_MAP, DISCOUNT_CODES_TEXT } from './constants';
 import styles from './discount-codes.module.css';
 
+const EVEN_CHECK_MODULO = 2;
+
 export class DiscountCodesView extends BaseComponent implements Component {
   private readonly headingElement = h2({ className: styles.heading }, DISCOUNT_CODES_TEXT.HEADING);
 
@@ -21,12 +23,20 @@ export class DiscountCodesView extends BaseComponent implements Component {
   public createHTML(codes: AppDiscountCode[]): void {
     this.append(this.headingElement);
 
-    for (const code of codes) {
-      this.append(this.createDiscountCodeCart(code));
+    for (const [index, code] of Object.entries(codes)) {
+      this.append(
+        this.createDiscountCodeCart(
+          code,
+          Number.parseInt(index) % EVEN_CHECK_MODULO === 0 ? 'right' : 'left',
+        ),
+      );
     }
   }
 
-  private createDiscountCodeCart(code: AppDiscountCode): HTMLDivElement {
+  private createDiscountCodeCart(
+    code: AppDiscountCode,
+    imagePosition: 'left' | 'right',
+  ): HTMLDivElement {
     const nameElement = h3({ className: styles.name }, code.name);
 
     const descriptionElement = p({ className: styles.description }, code.description);
@@ -38,7 +48,7 @@ export class DiscountCodesView extends BaseComponent implements Component {
     });
 
     return div(
-      { className: styles.cardContainer },
+      { className: [styles.cardContainer, styles[imagePosition]] },
       imageElement,
       nameElement,
       descriptionElement,
