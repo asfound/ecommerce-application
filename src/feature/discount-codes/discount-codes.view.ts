8b9fd3c57c6093222417ab8605c918a80f26708e
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2025-06-08 21:58:11 +0500

    feat: add codes to main page

--- src/feature/discount-codes/discount-codes.view.ts
+++ src/feature/discount-codes/discount-codes.view.ts
@@ -2,6 +2,6 @@ import { BaseComponent } from '~/components/base-component/base-component';
 
 export class DiscountCodesView extends BaseComponent {
   public constructor() {
-    super({ className: 'DISCOUNT-CODES', tagName: 'div' });
+    super({ className: 'DISCOUNT-CODES', tagName: 'div', textContent: 'Codes' });
   }
 }
