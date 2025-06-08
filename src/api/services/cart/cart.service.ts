81fc9acbac08fb54f22fcab39c184ca7fb2bf177
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2025-06-08 21:47:22 +0500

    feat: add getDiscountCodes method

--- src/api/services/cart/cart.service.ts
+++ src/api/services/cart/cart.service.ts
@@ -1,4 +1,9 @@
-import type { Cart, ClientResponse, MyCartUpdateAction } from '@commercetools/platform-sdk';
+import type {
+  Cart,
+  ClientResponse,
+  DiscountCodePagedQueryResponse,
+  MyCartUpdateAction,
+} from '@commercetools/platform-sdk';
 
 import type { ApiRootGetter } from '~/api/types/types';
 
@@ -59,6 +64,10 @@ export class CartService {
     } catch {
       return await this.createCart();
     }
+  }
+
+  public async getDiscountCodes(): Promise<ClientResponse<DiscountCodePagedQueryResponse>> {
+    return await this.apiRoot().discountCodes().get().execute();
   }
 
   public async getProductsSkuSet(): Promise<Set<string>> {
