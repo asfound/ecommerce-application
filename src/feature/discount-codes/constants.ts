7d17e654e76a69b3b7a0c4b475f9c0b59367b4a5
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2025-06-10 15:27:20 +0500

    chore: run format

--- src/feature/discount-codes/constants.ts
+++ src/feature/discount-codes/constants.ts
@@ -17,7 +17,6 @@ export const DISCOUNT_CODE_NOTIFICATION = {
   CODE_COPIED: (code: string): string => `Discount code "${code}" copied to clipboard`,
 } as const;
 
-
 export const DISCOUNT_CODE_ERROR = {
   FAILED: 'Failed to load promo codes',
 } as const;
