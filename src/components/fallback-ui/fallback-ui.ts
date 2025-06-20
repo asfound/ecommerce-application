d820d07271c5adc5603d462508cb852e8668094e
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2025-06-20 23:08:00 +0500

    fix: add button to fallback ui

--- src/components/fallback-ui/fallback-ui.ts
+++ src/components/fallback-ui/fallback-ui.ts
@@ -3,6 +3,7 @@ import gifHUH from '~/assets/img/huh-cat.gif';
 import { h1, img } from '~/shared/create-element/tags';
 
 import { BaseComponent } from '../base-component/base-component';
+import { Button } from '../common/button/button';
 import styles from './fallback-ui.module.css';
 
 export class FallbackUI extends BaseComponent {
@@ -12,6 +13,14 @@ export class FallbackUI extends BaseComponent {
     this.append(
       h1(null, APP_ERROR_MESSAGE.FAILED_TO_INITIALIZE),
       img({ className: styles.gif, src: gifHUH }),
+      new Button({
+        className: styles.button,
+        onClick: (): void => {
+          globalThis.location.reload();
+        },
+        textContent: 'Reload page',
+        type: 'button',
+      }),
     );
   }
 }
